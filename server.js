eca8ddb8ffbe2ac987e35168ef9446b8d9b4372b
Gaurav Parmar <gsp17feb@gmail.com>
2018-03-10 15:24:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -44,7 +44,187 @@ wss.on('connection', function(connection) {
 	 
    //when server gets a message from a connected user 
    connection.on('message', function(message) {
+	 'use strict';
+
+const express = require('express');
+const SocketServer = require('ws').Server;
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
+
+const server = express()
+ // .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const wss = new SocketServer({ server });
+
+// wss.on('connection', (ws) => {
+//   console.log('Client connected');
+//   ws.on('close', () => console.log('Client disconnected'));
+// });
+
+// setInterval(() => {
+//   wss.clients.forEach((client) => {
+//     client.send(new Date().toTimeString());
+//   });
+// }, 1000);
+
+//all connected to the server users 
+var users = {};
+
+var group = {};
+group["room"]={};
+group.room["gaurav"] = "connection gaurav";
+
+group.room["nikhil"] = "connection nikhil";
+console.log("test 2d array "+group.room.nikhil);
+console.log("test 2d array "+group["room"]["nikhil"]);
+
+  
+//when a user connects to our sever 
+wss.on('connection', function(connection) {
+  
+   console.log("User connected");
 	 
+   //when server gets a message from a connected user 
+   connection.on('message', function(message) {
+	 console.log("User message "+message);
+      var data; 
+      //accepting only JSON messages 
+      try { 
+         data = JSON.parse(message); 
+      } catch (e) { 
+         console.log("Invalid JSON"); 
+         data = {}; 
+      }
+		  
+      //switching type of the user message 
+      switch (data.type) { 
+         //when a user tries to login 
+         case "login": 
+            console.log("User logged", data.name); 
+            //if anyone is logged in with this username then refuse 
+            if(users[data.name]) { 
+               sendTo(connection, { 
+                  type: "login", 
+                  success: false 
+               }); 
+            } else { 
+               //save user connection on the server 
+               users[data.name] = connection; 
+               connection.name = data.name; 
+					
+               sendTo(connection, { 
+                  type: "login", 
+                  success: true 
+               }); 
+            }
+				
+            break;
+				
+         case "offer": 
+            //for ex. UserA wants to call UserB 
+            console.log("Sending offer to: ", data.name); 
+				
+            //if UserB exists then send him offer details 
+            var conn = users[data.name]; 
+				
+            if(conn != null) { 
+               //setting that UserA connected with UserB 
+               connection.otherName = data.name; 
+					
+               sendTo(conn, { 
+                  type: "offer", 
+                  offer: data.offer, 
+                  name: connection.name 
+               }); 
+            } 
+				
+            break;
+				
+         case "answer": 
+            console.log("Sending answer to: ", data.name); 
+            //for ex. UserB answers UserA 
+            var conn = users[data.name]; 
+				
+            if(conn != null) { 
+               connection.otherName = data.name; 
+               sendTo(conn, { 
+                  type: "answer", 
+                  answer: data.answer 
+               }); 
+            } 
+				
+            break;
+				
+         case "candidate": 
+            console.log("Sending candidate to:",data.name);
+            var conn = users[data.name];  
+				
+            if(conn != null) { 
+               sendTo(conn, { 
+                  type: "candidate", 
+                  candidate: data.candidate 
+               }); 
+            } 
+				
+            break;
+				
+         case "leave": 
+            console.log("Disconnecting from", data.name); 
+            var conn = users[data.name]; 
+            conn.otherName = null; 
+				
+            //notify the other user so he can disconnect his peer connection 
+            if(conn != null) { 
+               sendTo(conn, { 
+                  type: "leave"
+               });
+            }  
+				
+            break;
+				
+         default: 
+            sendTo(connection, { 
+               type: "error", 
+               message: "Command not found: " + data.type 
+            }); 
+				
+            break;
+				
+      }  
+   });
+	
+   //when user exits, for example closes a browser window 
+   //this may help if we are still in "offer","answer" or "candidate" state 
+   connection.on("close", function() { 
+	
+      if(connection.name) { 
+         delete users[connection.name]; 
+			
+         if(connection.otherName) { 
+            console.log("Disconnecting from ", connection.otherName); 
+            var conn = users[connection.otherName]; 
+            conn.otherName = null;
+				
+            if(conn != null) { 
+               sendTo(conn, { 
+                  type: "leave" 
+               }); 
+            }  
+         } 
+      } 
+   });
+	
+   connection.send("Hello world");
+	
+});
+  
+function sendTo(connection, message) { 
+   connection.send(JSON.stringify(message)); 
+}
+
       var data; 
       //accepting only JSON messages 
       try { 
