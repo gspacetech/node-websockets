3b462017213e726179c3b9930de80951656ea3fb
Gaurav Parmar <gsp17feb@gmail.com>
2018-03-10 12:01:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ group.room["gaurav"] = "connection";
 
 group.room["nikhil"] = "connection1";
 console.log("test 2d array "+group.room.nikhil);
-console.log("test 2d array "+group.room.length);
+console.log("test 2d array "+group["room"]["nikhil"]);
 console.log("test 2d array "+group[0][1]);
   
 //when a user connects to our sever 
